d913133af7d54eac152a6510684d96932baa433b
Svitlana Zaster <szaster@protonmail.com>
2020-08-18 18:17:54 -0500

    added connection string

--- server.js
+++ server.js
@@ -15,7 +15,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+let uri = "mongodb://localhost/budget";
+if (process.env.NODE_ENV === "production") {
+  uri = process.env.MONGODB_URI;
+}
+
+mongoose.connect(uri, {
   useNewUrlParser: true,
   useFindAndModify: false,
 });
